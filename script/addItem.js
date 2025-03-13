d88d1d6fc47835193c5d2d1b4758fb50ba2ee192
Martijn van Boven <info@martijnvanboven.nl>
2025-03-13 09:21:13 +0100

    Gerechten make-over oeleh

--- script/addItem.js
+++ script/addItem.js
@@ -5,11 +5,7 @@ document.querySelectorAll('.toevoegen').forEach(knop => {
     const id = this.id;    
     
     items.push(id);
-    
-    console.log(items);
     updateList(items);
-    
-    // const prijs = document.querySelector(`.${id}`).querySelector('.prijs').innerHTML;
     
     let origineleTekst = knop.value;
     knop.value = "Toegevoegd!";
@@ -20,14 +16,20 @@ document.querySelectorAll('.toevoegen').forEach(knop => {
   })
 })
 
-document.querySelector('.items-list').addEventListener('click', function(event) {
+document.querySelector('.items-table').addEventListener('click', function(event) {
   if (event.target.classList.contains('delete')) {
-    items.splice(items.lastIndexOf(event.target.classList[1]));
-    console.log('deleted')
+    items.splice(items.lastIndexOf(event.target.classList[0]), 1);
+    updateList(items);
+    console.log(event.target.classList[0]);
+  }
+
+  if (event.target.classList.contains('add')) {
+    items.push(event.target.classList[0]);
     updateList(items);
   }
-});
 
+  console.log(items);
+});
 
 function updateList(arr = []) {
   const counts = {};
@@ -36,19 +38,53 @@ function updateList(arr = []) {
     counts[num] = counts[num] ? counts[num] + 1 : 1;
   } 
   
-  const items = document.querySelector('.items-list');
-  items.innerHTML = '';
+  const items = document.querySelector('.items-table');
+  items.innerHTML = '<tr><th>Aantal</th><th>Item</th><th>Prijs</th></tr>';
   
   Object.entries(counts).forEach(([key, value]) => {
-    console.log(key);
     const title = document.querySelector(`.${key}`).closest('.card').querySelector('.titel').innerHTML.trim();
+    const prijs = parseFloat(document.querySelector(`.${key}`).querySelector('.prijs').innerHTML.split(' ')[1]) * value;
 
-    const li = document.createElement('li');
-    li.className = `item ${title}`;
+    const tr = document.createElement('tr');
+    tr.className = `item ${key}`;
     
-    li.innerHTML = `<p>${value}x ${title}</p> <a href='#' class='delete ${key}' onclick='return false;'>×</a>`;
-    items.appendChild(li);
-    // console.log(`${title}: ${value}`);
+    const addLink = `<a href='#' class='${key} add' onclick='return false;'> + </a>`;
+    const deleteLink = `<a href='#' class='${key} delete' onclick='return false;'> - </a>`;
+
+    tr.innerHTML = `<td>${value}x <span>${addLink} ${deleteLink}</span></td> <td>${title}</td> <td>€ ${prijs.toFixed(2)}</td>`;
+    items.appendChild(tr);
+
   });
+
+  const totaalPrijs = calculateTotalPrice(counts);
+  document.querySelector('.totaal-prijs').innerHTML = `Totaalprijs: €${totaalPrijs}`;
 }
 
+function calculateTotalPrice(counts) {
+  if (!Object.keys(counts).length) return "-,--";
+
+  let price = 0;
+  Object.entries(counts).forEach(([key, value]) => {
+    const itemPrice = parseFloat(document.querySelector(`.${key}`).querySelector('.prijs').innerHTML.split(' ')[1]) * value;
+
+    console.log(itemPrice, value);
+
+    price += itemPrice;
+  });
+
+  return price.toFixed(2);
+}
+
+
+const toonMeerKnop = document.getElementById("toonMeer");
+const cards = document.querySelectorAll('.card');
+const cardsToToggle = Array.from(cards).slice(-14); // Neem de laatste 14 kaarten
+
+toonMeerKnop.addEventListener("click", function () {
+  cardsToToggle.forEach(card => {
+    card.style.display = (card.style.display === "flex") ? "none" : "flex";
+  });
+
+  const allesZichtbaar = Array.from(cardsToToggle).some(card => card.style.display === "flex");
+  toonMeerKnop.innerHTML = allesZichtbaar ? 'Toon minder <i class="bi bi-caret-up"></i>' : 'Toon meer <i class="bi bi-caret-down"></i>';
+});
